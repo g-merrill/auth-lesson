b0c46b1269418ae903a17b510fc1ea26c27c877c
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-06-21 14:14:17 -0700

    wip

--- frontend/src/components/01-Hashing.jsx
+++ frontend/src/components/01-Hashing.jsx
@@ -4,18 +4,45 @@ import md5 from 'md5'
 export default function Hashing() {
   const [hashes, setHashes] = useState([])
   const inputRef = useRef(null)
+
   useEffect(() => {
-    md5('something')
-  }, [])
-  const onHash = () => {
-    setHashes(hashes.concat(md5(inputRef.current.value)))
+    console.log(hashes.at(-1))
+  }, [hashes])
+
+  const onHash = evt => {
+    evt.preventDefault()
+    const original = inputRef.current.value
+    const hash = md5(original)
+    setHashes(hashes.concat([[original, hash]]))
   }
   return (
     <div className="container">
       <h2>Hashing</h2>
-      <input ref={inputRef} placeholder='type something' />
-      <button onClick={onHash}>Hash It!</button>
-      {hashes.map((hash, i) => <p className='hash' key={i}>{hash}</p>)}
+      <form>
+        <label>
+          Value to Hash:
+          <input ref={inputRef} placeholder='type something' />
+          <button onClick={onHash}>Hash It!</button>
+        </label>
+      </form>
+      {hashes.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>Original</th>
+              <th>MD5 Hash</th>
+            </tr>
+          </thead>
+          <tbody>
+            {hashes.map((hash, i) => (
+              <tr key={i}>
+                <td>{hash[0]}</td>
+                <td>{hash[1]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   )
 }
