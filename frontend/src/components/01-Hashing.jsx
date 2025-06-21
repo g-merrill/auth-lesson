8ae0f65c8e00d3e2b3630ba34cc4471661d943db
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-06-21 13:38:14 -0700

    style

--- frontend/src/components/01-Hashing.jsx
+++ frontend/src/components/01-Hashing.jsx
@@ -2,20 +2,20 @@ import { useState, useEffect, useRef } from 'react'
 import md5 from 'md5'
 
 export default function Hashing() {
-  const [hash, setHash] = useState('')
+  const [hashes, setHashes] = useState([])
   const inputRef = useRef(null)
   useEffect(() => {
     md5('something')
   }, [])
   const onHash = () => {
-    setHash(md5(inputRef.current.value))
+    setHashes(hashes.concat(md5(inputRef.current.value)))
   }
   return (
     <div className="container">
       <h2>Hashing</h2>
       <input ref={inputRef} placeholder='type something' />
       <button onClick={onHash}>Hash It!</button>
-      <div>The hash: {hash}</div>
+      {hashes.map((hash, i) => <p className='hash' key={i}>{hash}</p>)}
     </div>
   )
 }
