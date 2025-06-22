c30efeadac896e04de8d4f1cda93f91737d15c9e
Gabriel Cabrejas <gabriel@cabrejas.net>
2025-06-21 19:53:11 -0700

    wip

--- frontend/src/components/02-Register.jsx
+++ frontend/src/components/02-Register.jsx
@@ -1,7 +1,35 @@
+import { useState, useEffect, useRef } from 'react'
+
 export default function Register() {
+  const [message, setMessage] = useState('')
+  const userRef = useRef(null)
+  const passRef = useRef(null)
+
+  const onSubmit = evt => {
+    const action = evt.nativeEvent.submitter.name
+    evt.preventDefault()
+    const username = userRef.current.value
+    const password = passRef.current.value
+    const register = async () => {
+      const res = await fetch(`/api/auth/${action}`, {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      })
+      const parsed = await res.json()
+      setMessage(parsed.message)
+    }
+    register()
+  }
   return (
-    <div className="container">
-      <h2>Register</h2>
+    <div className='container'>
+      <h2>{message}</h2>
+      <form onSubmit={onSubmit}>
+        <input ref={userRef} />
+        <input ref={passRef} />
+        <button name="register">register</button>
+        <button name="login">login</button>
+      </form>
     </div>
   )
 }
--- routes/auth.js
+++ routes/auth.js
@@ -12,9 +12,9 @@ router.post('/api/auth/register', async (req, res, next) => {
     const hash = await hashPassword(plainPassword)
     const newUser = { id: getId(), username, password: hash }
     users.push(newUser)
-    res.json(newUser)
+    res.json({ message: `Hey ${username}, you registered successfully` })
   } else {
-    next({ status: 409, message: 'username taken' })
+    next({ status: 409, message: 'Username taken' })
   }
 })
 
@@ -22,13 +22,14 @@ router.post('/api/auth/login', async (req, res, next) => {
   const { username, password: plainPassword } = req.body
   const user = users.find(u => u.username === username)
   if (!user) {
-    next({ status: 401, message: 'invalid credentials' })
+    next({ status: 401, message: 'Invalid credentials' })
   } else {
     const isValid = await verifyPassword(user.password, plainPassword)
     if (isValid) {
-      res.json('YOU ARE LOGGED IN')
+      req.session.user = user
+      res.json({ message: `Welcome back ${username}, have a cookie...` })
     } else {
-      next({ status: 401, message: 'invalid credentials' })
+      next({ status: 401, message: 'Invalid credentials' })
     }
   }
 })
