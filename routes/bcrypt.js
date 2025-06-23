66091d9d7ad8cc9b624f4c30052b90b1a51092cb
Greg Merrill <gmerrill03@gmail.com>
2025-06-23 11:45:21 -0700

    define bcrypt methods

--- routes/bcrypt.js
+++ routes/bcrypt.js
@@ -1,13 +1,27 @@
+const bcrypt = require('bcryptjs')
+
 async function hashPassword(plainPassword) {
-  // on success return hash
-  // on error throw
-  throw new Error('hashPassword not implemented!')
+  try {
+    // on success return hash
+    const hash = await bcrypt.hash(plainPassword, 10)
+    return hash
+  } catch (error) {
+    // on error throw
+    console.log('Hashing went wrong')
+    throw new Error('hashPassword not implemented!')
+  }
 }
 
 async function verifyPassword(plainPassword, hash) {
-  // on valid password return true
-  // on invalid return false
-  return false
+  try {
+    // on valid password return true
+    await bcrypt.compare(plainPassword, hash)
+    return true
+  } catch (error) {
+    // on invalid return false
+    
+    return false
+  }
 }
 
 module.exports = { hashPassword, verifyPassword }
